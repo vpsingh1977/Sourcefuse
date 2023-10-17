ebf6bb39753d7f4e05b2320ef2d2e96c3fa768d0
vpsingh1977 <148262532+vpsingh1977@users.noreply.github.com>
2023-10-17 14:53:02 -0400

    Add files via upload

--- taskdef.tf
+++ taskdef.tf
@@ -10,7 +10,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
  container_definitions = jsonencode([
    {
      name      = "nginx"
-     image     = "public.ecr.aws/nginx/nginx:stable-perl:latest"
+     image     = "public.ecr.aws/nginx/nginx:latest"
      cpu       = 256
      memory    = 512
      essential = true
